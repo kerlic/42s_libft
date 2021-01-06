5f4f6178bb0c5fee502330cfb93f50e594045456
home-desktop <kerli@naver.com>
2021-01-06 21:12:04 +0900

    trimfix

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ilsong <ilsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 21:36:17 by ilsong            #+#    #+#             */
-/*   Updated: 2021/01/06 19:58:17 by ilsong           ###   ########.fr       */
+/*   Updated: 2021/01/06 21:10:23 by ilsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,15 @@ static	size_t	ft_trmlen(char const *s1, char const *set)
 {
 	char	const	*start;
 	char	const	*end;
-
+	
 	while (is_set(s1++, set))
 		;
+	if (*(s1 - 1) == '\0')
+		return (0);
 	start = s1 - 1;
 	while (*s1)
 		s1++;
-	while (is_set(--s1, set))
+	while (is_set(--s1, set) && start <= s1)
 		;
 	end = s1;
 	return ((size_t)(end - start));
@@ -38,15 +40,17 @@ static	size_t	ft_trmlen(char const *s1, char const *set)
 
 char			*ft_strtrim(char const *s1, char const *set)
 {
-	size_t		len_s1;
+	size_t		trlen;
 	char		*trmd_str;
 
-	len_s1 = ft_trmlen(s1, set);
-	if (!(trmd_str = (char *)malloc(sizeof(char) * (len_s1 + 2))))
-		return (0);
+	trlen = ft_trmlen(s1, set);
+	if (!trlen)
+		return ("");
+	if (!(trmd_str = (char *)malloc(sizeof(char) * (trlen + 2))))
+			return (0);
 	while (is_set(s1, set))
 		++s1;
-	ft_memmove(trmd_str, s1, len_s1 + 1);
-	trmd_str[len_s1 + 1] = 0;
+	ft_memmove(trmd_str, s1, trlen + 1);
+	trmd_str[trlen + 1] = 0;
 	return (trmd_str);
 }
